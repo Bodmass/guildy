da37d9627298b291a85755e914e9db58e1e4171b
Aziz Arar <ararwow@gmail.com>
2020-10-08 14:14:04 +0100

    chore: run prettier

--- components/hooks/useFathom.ts
+++ components/hooks/useFathom.ts
@@ -1,31 +1,31 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as Fathom from "fathom-client";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
 
-const ID = '';
+const ID = ''
 
 export default function useFathom() {
-    const router = useRouter();
-    useEffect(() => {
-        if (!ID) {
-            console.warn('[warn]', 'No Fathom ID set');
-            return;
-        }
+  const router = useRouter()
+  useEffect(() => {
+    if (!ID) {
+      console.warn('[warn]', 'No Fathom ID set')
+      return
+    }
 
-        // Initialize Fathom when the app loads
-        Fathom.load(ID, {
-            excludedDomains: ["localhost"],
-        });
+    // Initialize Fathom when the app loads
+    Fathom.load(ID, {
+      excludedDomains: ['localhost'],
+    })
 
-        function onRouteChangeComplete() {
-            Fathom.trackPageview();
-        }
-        // Record a pageview when route changes
-        router.events.on("routeChangeComplete", onRouteChangeComplete);
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
 
-        // Unassign event listener
-        return () => {
-            router.events.off("routeChangeComplete", onRouteChangeComplete);
-        };
-    }, []);
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import "./styles.css";
-import Head from "next/head";
-import { DefaultSeo } from "next-seo";
-import useFathom from "../components/hooks/useFathom";
-import SEO from "../next-seo.config";
+import './styles.css'
+import Head from 'next/head'
+import { DefaultSeo } from 'next-seo'
+import useFathom from '../components/hooks/useFathom'
+import SEO from '../next-seo.config'
 
 function App({ Component, pageProps }) {
-  useFathom();
+  useFathom()
   return (
     <>
       <Head>
@@ -13,13 +13,13 @@ function App({ Component, pageProps }) {
         <meta name="theme-color" content="#fd015d" />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22> <text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>" 
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22> <text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
         />
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
