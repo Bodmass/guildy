d24fe8bd1360aa76385e3983710cc5c68529424b
Aziz <ararwow@gmail.com>
2020-10-09 17:03:32 +0100

    feat: add button and profile style

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,25 @@ import styles from './header.module.css'
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <div className={styles.h1}>[Image] Character Name</div>
+    <div>
+      <div className={styles.header}>
+        <div className={styles.profile}>
+          <div className={styles.profileimage} />
+          Character Name
+        </div>
+        <div className={styles.profile}>
+          <div className={styles.profileimage} />
+          Guild
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.button}>?</div>
+          <div className={styles.button}>
+            <a className={styles.buttonhover} href="#">
+              ⚙
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
