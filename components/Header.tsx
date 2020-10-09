a227455e1424e1c9ad8a115083efa6054f2b462c
Aziz <ararwow@gmail.com>
2020-10-10 00:18:31 +0100

    feat: add icons, add text scaling clamp

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { FaBell, FaCog, FaCaretDown } from 'react-icons/fa'
 import styles from './header.module.css'
 
 const Header = () => {
@@ -5,18 +6,28 @@ const Header = () => {
     <div>
       <div className={styles.header}>
         <div className={styles.profile}>
-          <div className={styles.profileimage} />
-          Character Name
+          <div className={styles.flex}>
+            <div className={styles.profileimage} />
+            <div className={styles.profilename}>Character Name</div>
+          </div>
+          <div className={styles.caret}>
+            <FaCaretDown />
+          </div>
         </div>
+
         <div className={styles.profile}>
-          <div className={styles.profileimage} />
-          Guild
+          <div className={styles.flex}>
+            <div className={styles.profileimage} />
+            <div className={styles.profilename}>Guild Name</div>
+          </div>
         </div>
         <div className={styles.buttons}>
-          <div className={styles.button}>?</div>
+          <div className={styles.button}>
+            <FaBell />
+          </div>
           <div className={styles.button}>
             <a className={styles.buttonhover} href="#">
-              ⚙
+              <FaCog />
             </a>
           </div>
         </div>
