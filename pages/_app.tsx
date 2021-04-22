44165cc4d4cfd13f89cd2b2b3cfc5e1bdafb5068
Bodmass <aziz.n.arar@gmail.com>
2021-04-22 10:53:32 +0100

    feat: add profile and character to index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import { DefaultSeo } from 'next-seo'
 import useFathom from '../components/hooks/useFathom'
 import SEO from '../next-seo.config'
+import './calendar.css'
 
 function App({ Component, pageProps }) {
   useFathom()
@@ -11,10 +12,7 @@ function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#fd015d" />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22> <text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
-        />
+        <link rel="icon" href="/images/icon.svg" />
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
-import { useMemo } from 'react'
+import { useMemo, useEffect, useState } from 'react'
+import { parseCookies } from 'nookies'
 import BnetLogin from '../components/BnetLogin'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import BnetProfileList from '../components/BnetProfileList'
+import GuildyCalendar from '../components/GuildyCalendar'
 
 const bgArray = [
   'maw1.jpg',
@@ -20,8 +23,62 @@ export default function Index() {
   const background = useMemo(() => {
     return bgArray[Math.floor(Math.random() * bgArray.length)]
   }, [])
+
+  const [loginStatus, setLoginStatus] = useState(null)
+  const [characterStatus, setCharacterStatus] = useState(null)
+  const cookiesId = parseCookies().id
+  const cookiesCharacter = parseCookies().character
+
+  useEffect(() => {
+    if (cookiesId === '{}') {
+      // notloggedin
+    } else {
+      setLoginStatus(cookiesId)
+    }
+
+    if (cookiesCharacter === '{}') {
+      // notloggedin
+    } else {
+      setCharacterStatus(cookiesCharacter)
+    }
+  }, [])
+
+  if (loginStatus === undefined) {
+    return (
+      <>
+        <style jsx global>
+          {`
+            body {
+              background-color: black;
+              background: no-repeat center/cover url(../images/backgrounds/${background});
+            }
+          `}
+        </style>
+        <BnetLogin />
+        <Footer />
+      </>
+    )
+  }
+
+  if (characterStatus === undefined) {
+    return (
+      <>
+        <style jsx global>
+          {`
+            body {
+              background: no-repeat center/cover url(../images/backgrounds/${background});
+            }
+          `}
+        </style>
+        <Header />
+        <BnetProfileList />
+        <Footer />
+      </>
+    )
+  }
+
   return (
-    <div>
+    <>
       <style jsx global>
         {`
           body {
@@ -29,9 +86,10 @@ export default function Index() {
           }
         `}
       </style>
+
       <Header />
-      <BnetLogin />
+      <GuildyCalendar />
       <Footer />
-    </div>
+    </>
   )
 }
